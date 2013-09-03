e9e33fe8cd0416722535f8fe079608902683c496
Dean Foster <directorchurch2012@icloud.com>
2013-09-03 19:45:28 +1000

    Created a rails application and added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hacknet::Application.config.secret_token = 'c14dcdfa152d3c2abbd2dd06fdf2b1f471c6886e2cac5cafbb9b8f84ba1f7e6020a8cfc49796c21923474285a2548e5c1e18ef8764722de7176819892ea31e5b'
